d5d3d1ca4655cbfc1c953bee64964637be7fc47d
umaismateen <umais.mateen@gmail.com>
2023-08-06 09:56:38 +0500

    remove unwanted files

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { useToast } from "@chakra-ui/react";
+import HeroSection from "./components/HeroSection";
+import Characters from "./components/Characters";
+import Footer from "./components/Footer";
 
 function App() {
+  const [isOnline, setIsOnline] = useState(navigator.onLine);
+  const toast = useToast();
+
+  useEffect(() => {
+    const handleStatusChange = () => {
+      setIsOnline(navigator.onLine);
+      if (navigator.onLine) {
+        toast({
+          position: "top",
+          title: "You are online.",
+          status: "success",
+          duration: 9000,
+          isClosable: true,
+        });
+      } else {
+        toast({
+          position: "top",
+          title: "You are offline.",
+          description: "Please turn on your Wi-Fi to connect.",
+          status: "error",
+          duration: 9000,
+          isClosable: true,
+        });
+      }
+    };
+
+    // Listen to the online status
+    window.addEventListener("online", handleStatusChange);
+
+    // Listen to the offline status
+    window.addEventListener("offline", handleStatusChange);
+
+    // Specify how to clean up after this effect for performance improvment
+    return () => {
+      window.removeEventListener("online", handleStatusChange);
+      window.removeEventListener("offline", handleStatusChange);
+    };
+  }, [isOnline, toast]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <HeroSection />
+      <Characters />
+      <Footer />
+    </>
   );
 }
 
